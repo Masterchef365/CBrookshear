ceb17115fab0daef08eadde150ba5d7326e85109
Masterchef365 <duncan.freeman1@gmail.com>
2019-04-15 00:39:00 -0700

    Remove cruft, add a compare command

--- main.c
+++ main.c
@@ -6,8 +6,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-unsigned char mem[0xFF] = {0};
-unsigned char reg[0x0F] = {0};
+unsigned char mem[0x100] = {0};
+unsigned char reg[0x10] = {0};
 unsigned char program_counter = 0;
 
 struct Instruction {
@@ -15,8 +15,8 @@ struct Instruction {
     unsigned char opcode : 4; // High split
     union {
         struct {
-            unsigned char hi : 4; // Low split
-            unsigned char lo : 4; // High split
+            unsigned char lo : 4; // Low split
+            unsigned char hi : 4; // High split
         } split;
         unsigned char byte : 8;
     } args;
@@ -36,7 +36,7 @@ int main (int argc, char** argv) {
     if (argc < 2) return EXIT_FAILURE;
     int fd = open(argv[1], O_RDONLY);
     if (fd < 0) return EXIT_FAILURE;
-    if (read(fd, mem, 0xFF) == -1) return EXIT_FAILURE;
+    if (read(fd, mem, 0x100) == -1) return EXIT_FAILURE;
     close(fd);
     while (step());
     putchar('\n');
@@ -50,7 +50,7 @@ int step() {
     fprintf(stderr, " PC: %02hhX", program_counter);
     fprintf(stderr, " OP: %c", nibble_to_hex(instruction.opcode));
     fprintf(stderr, " RG: %c", nibble_to_hex(instruction.reg));
-    fprintf(stderr, " NIB: [A:%c, B:%c]", nibble_to_hex(instruction.args.split.lo), nibble_to_hex(instruction.args.split.hi));
+    fprintf(stderr, " NIB: [A:%c, B:%c]", nibble_to_hex(instruction.args.split.hi), nibble_to_hex(instruction.args.split.lo));
     fprintf(stderr, " BYTE: %02hhx\n", instruction.args.byte);
     fprintf(stderr, "  ");
 
@@ -72,44 +72,44 @@ int step() {
             break;
         case 0x4: // Move/Copy register 
             fprintf(stderr, "Move reg %c (%02hhX) to reg %c (%02hhX)\n", 
-                    nibble_to_hex(instruction.args.split.lo),
-                    reg[instruction.args.split.lo],
                     nibble_to_hex(instruction.args.split.hi),
-                    reg[instruction.args.split.hi]);
-            reg[instruction.args.split.hi] = 
-                reg[instruction.args.split.lo];
+                    reg[instruction.args.split.hi],
+                    nibble_to_hex(instruction.args.split.lo),
+                    reg[instruction.args.split.lo]);
+            reg[instruction.args.split.lo] = 
+                reg[instruction.args.split.hi];
             break;
         case 0x5: // Add
             fprintf(stderr, "Add reg %c (%02hhX) + reg %c (%02hhX) -> reg %c (%02hhX)\n", 
-                    nibble_to_hex(instruction.args.split.lo),
-                    reg[instruction.args.split.lo],
                     nibble_to_hex(instruction.args.split.hi),
                     reg[instruction.args.split.hi],
+                    nibble_to_hex(instruction.args.split.lo),
+                    reg[instruction.args.split.lo],
                     nibble_to_hex(instruction.reg),
-                    (unsigned char)(reg[instruction.args.split.lo] + reg[instruction.args.split.hi]));
+                    (unsigned char)(reg[instruction.args.split.hi] + reg[instruction.args.split.lo]));
             reg[instruction.reg] = 
-                reg[instruction.args.split.lo] + 
-                reg[instruction.args.split.hi];
+                reg[instruction.args.split.hi] + 
+                reg[instruction.args.split.lo];
             break;
         case 0x7: // Or
             reg[instruction.reg] = 
-                reg[instruction.args.split.lo] | 
-                reg[instruction.args.split.hi];
+                reg[instruction.args.split.hi] | 
+                reg[instruction.args.split.lo];
             break;
         case 0x8: // And
             reg[instruction.reg] = 
-                reg[instruction.args.split.lo] & 
-                reg[instruction.args.split.hi];
+                reg[instruction.args.split.hi] & 
+                reg[instruction.args.split.lo];
             break;
         case 0x9: // Xor
             reg[instruction.reg] = 
-                reg[instruction.args.split.lo] != 
-                reg[instruction.args.split.hi];
+                reg[instruction.args.split.hi] != 
+                reg[instruction.args.split.lo];
             break;
         case 0xA: // Rotate right
             reg[instruction.reg] = 
                 reg[instruction.reg] >> 
-                instruction.args.split.hi;
+                instruction.args.split.lo;
             break;
         case 0xB: // Jump
             fprintf(stderr, "Jump to %02hhX\n if reg %c (%02hhX) == reg 0(%02hhX) ", 
@@ -131,14 +131,24 @@ int step() {
             fflush(stdout);
             fprintf(stderr, "]\n");
             break;
-        case 0xE:
-            fprintf(stderr, "Getchar to reg %c\n> ", nibble_to_hex(instruction.reg));
-            reg[instruction.reg] = getchar();
-            fprintf(stderr, "Got char %02hhX \n", reg[instruction.reg]);
+        case 0xE: //Compare/less
+            fprintf(stderr, "Set reg %c (%02hhX) to reg zero if reg %c (%02hhX) < reg %c (%02hhX)\n", 
+                    nibble_to_hex(instruction.reg),
+                    reg[instruction.reg],
+                    nibble_to_hex(instruction.args.split.hi),
+                    reg[instruction.args.split.hi],
+                    nibble_to_hex(instruction.args.split.lo),
+                    reg[instruction.args.split.lo]);
+            if (reg[instruction.args.split.hi] < reg[instruction.args.split.lo]) {
+                reg[instruction.reg] = reg[0];
+                fprintf(stderr, "\tTrue, set reg to reg0.\n");
+            } else {
+                fprintf(stderr, "\tFalse\n");
+            }
             break;
-        case 0xF:
+        case 0xF: //Debug register value
             fprintf(stderr, "Debug reg %c [ ", nibble_to_hex(instruction.reg));
-            printf("0x%02hhX", reg[instruction.reg]);
+            printf("0x%02hhX ", reg[instruction.reg]);
             fflush(stdout);
             fprintf(stderr, " ]\n");
             break;
